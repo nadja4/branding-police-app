9285088ac82eac7f751bc83ec3f9b858663e2b87
Nadja <>
2023-11-29 19:26:36 +0100

    Create SAS url to show the results

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import (Flask, redirect, render_template, request,
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
 from azure.storage.queue import QueueServiceClient, BinaryBase64EncodePolicy, BinaryBase64DecodePolicy
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import json
 
@@ -34,7 +34,7 @@ def upload_blob(file):
    file_urls = [blob_client.url]
 
    # Upload results_xx.txt
-   results_filename = "result_" + file.filename.split('.')[0] + ".txt"
+   results_filename = "results_" + file.filename.split('.')[0] + ".txt"
    results_f = open(results_filename, "w")
    results_f.write("Working on " + file.filename)
    results_f.close()
@@ -48,24 +48,43 @@ def upload_blob(file):
    os.remove(results_filename)
    return file_urls
 
-def get_sas_url():
-    # set client to access azure storage container
-    blob_service_client = BlobServiceClient(account_url=account_url_queue, credential=credentials)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob="results_test_pp.txt")
+def request_user_delegation_key(blob_service_client, start_time, expiry_time):
+    return blob_service_client.get_user_delegation_key(key_start_time=start_time,key_expiry_time=expiry_time)
 
-    # Gültigkeitsdauer der SAS-URL festlegen (hier: 1 Stunde)
+def create_user_delegation_sas_blob(blob_service_client, blob_client):
+    # Create a SAS token that's valid for one day, as an example
+    start_time = datetime.now(timezone.utc) - timedelta(minutes=1)
+    expiry_time = start_time + timedelta(hours=1)
+
+    user_delegation_key = request_user_delegation_key(blob_service_client, start_time, expiry_time)
+
+    print(blob_client.container_name, blob_client.blob_name, blob_client.account_name)
     sas_token = generate_blob_sas(
-        blob_client.account_name,
-        blob_client.container_name,
-        blob_client.blob_name,
-        credential=credentials,
+        account_name=blob_client.account_name,
+        container_name=blob_client.container_name,
+        blob_name=blob_client.blob_name,
+        user_delegation_key=user_delegation_key,
         permission=BlobSasPermissions(read=True),
-        expiry=datetime.utcnow() + timedelta(hours=1)
+        expiry=expiry_time,
+        start=start_time
     )
 
-    # Vollständige SAS-URL erstellen
+    return sas_token
+
+
+def get_sas_url():
+    # set client to access azure storage container
+    blob_service_client = BlobServiceClient(account_url=account_url_blob, credential=credentials)
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob="results_test_ppt.txt")
+    sas_token = create_user_delegation_sas_blob(blob_service_client, blob_client)      
+    # The SAS token string can be appended to the resource URL with a ? delimiter
+    # or passed as the credential argument to the client constructor
     sas_url = f"{blob_client.url}?{sas_token}"
-    return sas_url
+
+    # Create a BlobClient object with SAS authorization
+    blob_client_sas = blob_client.from_blob_url(blob_url=sas_url)
+    return blob_client_sas.url
 
 def queue(queue_content):
     queue_service_client = QueueServiceClient(account_url=account_url_queue, credential=credentials)
@@ -109,14 +128,13 @@ def upload_file():
    queue_content.append(search_for)
    queue(queue_content)
    print('Sent to queue')
-   link = file_urls[1]
-   return render_template('results.html', link=link)
+   return render_template('results.html', name=f.filename, text=search_for)
 
 @app.route('/results', methods = ['GET'])
 def link_to_file():
     print("Test")
-    #window.open("https://google.de")
-    #print(get_sas_url())
+    link = get_sas_url()
+    return redirect(link)
 
 if __name__ == '__main__':
    app.run()
